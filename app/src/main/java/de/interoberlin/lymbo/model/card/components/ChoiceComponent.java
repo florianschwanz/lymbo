e85384c275e7aed7fca83e083486cdc4b21a2fd0
Florian <florian.schwanz@interoberlin.de>
2014-12-11 22:58:59 +0100

    Added choice component to cards

--- app/src/main/java/de/interoberlin/lymbo/model/card/components/ChoiceComponent.java
+++ app/src/main/java/de/interoberlin/lymbo/model/card/components/ChoiceComponent.java
@@ -2,12 +2,18 @@ package de.interoberlin.lymbo.model.card.components;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import de.interoberlin.lymbo.R;
 import de.interoberlin.lymbo.model.Displayable;
 
 public class ChoiceComponent implements Displayable {
@@ -26,7 +32,29 @@ public class ChoiceComponent implements Displayable {
 
     @Override
     public View getView(Context c, Activity a, ViewGroup parent) {
-        return null;
+
+        LayoutInflater li = LayoutInflater.from(c);
+        TableLayout tlChoices = (TableLayout) li.inflate(R.layout.component_choice, null);
+
+        for (Answer answer : answers) {
+            LinearLayout llAnswer = (LinearLayout) li.inflate(R.layout.component_answer, null);
+
+            final CheckBox cb = (CheckBox) llAnswer.findViewById(R.id.cb);
+            final TextView tvText = (TextView) llAnswer.findViewById(R.id.tvText);
+
+            tvText.setText(answer.getValue());
+            tvText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    cb.toggle();
+                }
+            });
+
+
+            tlChoices.addView(llAnswer);
+        }
+
+        return tlChoices;
     }
 
     // --------------------
