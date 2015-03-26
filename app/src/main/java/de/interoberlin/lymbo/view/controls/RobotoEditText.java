23f48eff27cb488f8417eb9f2fa72f2b56cc609d
Florian <florian.schwanz@interoberlin.de>
2015-03-26 21:23:38 +0100

    Removed javadoc commments that caused errors

--- app/src/main/java/de/interoberlin/lymbo/view/controls/RobotoEditText.java
+++ app/src/main/java/de/interoberlin/lymbo/view/controls/RobotoEditText.java
@@ -10,10 +10,6 @@ import android.widget.EditText;
 import de.interoberlin.lymbo.R;
 
 public class RobotoEditText extends EditText {
-
-    /*
-     * Permissible values ​​for the "typeface" attribute.
-     */
     private final static int ROBOTO_THIN = 0;
     private final static int ROBOTO_THIN_ITALIC = 1;
     private final static int ROBOTO_LIGHT = 2;
@@ -30,67 +26,23 @@ public class RobotoEditText extends EditText {
     private final static int ROBOTO_CONDENSED_ITALIC = 13;
     private final static int ROBOTO_CONDENSED_BOLD = 14;
     private final static int ROBOTO_CONDENSED_BOLD_ITALIC = 15;
-    /**
-     * List of created typefaces for later reused.
-     */
+
     private final static SparseArray<Typeface> mTypefaces = new SparseArray<Typeface>(16);
 
-    /**
-     * Simple constructor to use when creating a view from code.
-     *
-     * @param context The Context the view is running in, through which it can
-     *                access the current theme, resources, etc.
-     */
     public RobotoEditText(Context context) {
         super(context);
     }
 
-    /**
-     * Constructor that is called when inflating a view from XML. This is called
-     * when a view is being constructed from an XML file, supplying attributes
-     * that were specified in the XML file. This version uses a default style of
-     * 0, so the only attribute values applied are those in the Context's Theme
-     * and the given AttributeSet.
-     * <p/>
-     * <p/>
-     * The method onFinishInflate() will be called after all children have been
-     * added.
-     *
-     * @param context The Context the view is running in, through which it can
-     *                access the current theme, resources, etc.
-     * @param attrs   The attributes of the XML tag that is inflating the view.
-     * @see #RobotoEditText(android.content.Context, android.util.AttributeSet, int)
-     */
     public RobotoEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
         parseAttributes(context, attrs);
     }
 
-    /**
-     * Perform inflation from XML and apply a class-specific base style. This
-     * constructor of View allows subclasses to use their own base style when
-     * they are inflating.
-     *
-     * @param context  The Context the view is running in, through which it can
-     *                 access the current theme, resources, etc.
-     * @param attrs    The attributes of the XML tag that is inflating the view.
-     * @param defStyle The default style to apply to this view. If 0, no style
-     *                 will be applied (beyond what is included in the theme). This may
-     *                 either be an attribute resource, whose value will be retrieved
-     *                 from the current theme, or an explicit style resource.
-     * @see #RobotoEditText(android.content.Context, android.util.AttributeSet)
-     */
     public RobotoEditText(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         parseAttributes(context, attrs);
     }
 
-    /**
-     * Parse the attributes.
-     *
-     * @param context The Context the view is running in, through which it can access the current theme, resources, etc.
-     * @param attrs   The attributes of the XML tag that is inflating the view.
-     */
     private void parseAttributes(Context context, AttributeSet attrs) {
         TypedArray values = context.obtainStyledAttributes(attrs, R.styleable.RobotoTextView);
 
@@ -100,15 +52,6 @@ public class RobotoEditText extends EditText {
         setTypeface(obtaintTypeface(context, typefaceValue));
     }
 
-    /**
-     * Obtain typeface.
-     *
-     * @param context       The Context the view is running in, through which it can
-     *                      access the current theme, resources, etc.
-     * @param typefaceValue values ​​for the "typeface" attribute
-     * @return Roboto {@link android.graphics.Typeface}
-     * @throws IllegalArgumentException if unknown `typeface` attribute value.
-     */
     private Typeface obtaintTypeface(Context context, int typefaceValue) throws IllegalArgumentException {
         Typeface typeface = mTypefaces.get(typefaceValue);
         if (typeface == null) {
@@ -118,15 +61,6 @@ public class RobotoEditText extends EditText {
         return typeface;
     }
 
-    /**
-     * Create typeface from assets.
-     *
-     * @param context       The Context the view is running in, through which it can
-     *                      access the current theme, resources, etc.
-     * @param typefaceValue values ​​for the "typeface" attribute
-     * @return Roboto {@link android.graphics.Typeface}
-     * @throws IllegalArgumentException if unknown `typeface` attribute value.
-     */
     private Typeface createTypeface(Context context, int typefaceValue) throws IllegalArgumentException {
         Typeface typeface;
         switch (typefaceValue) {
--- app/src/main/java/de/interoberlin/lymbo/view/controls/RobotoTextView.java
+++ app/src/main/java/de/interoberlin/lymbo/view/controls/RobotoTextView.java
@@ -10,10 +10,6 @@ import android.widget.TextView;
 import de.interoberlin.lymbo.R;
 
 public class RobotoTextView extends TextView {
-
-    /*
-     * Permissible values ​​for the "typeface" attribute.
-     */
     private final static int ROBOTO_THIN = 0;
     private final static int ROBOTO_THIN_ITALIC = 1;
     private final static int ROBOTO_LIGHT = 2;
@@ -30,67 +26,23 @@ public class RobotoTextView extends TextView {
     private final static int ROBOTO_CONDENSED_ITALIC = 13;
     private final static int ROBOTO_CONDENSED_BOLD = 14;
     private final static int ROBOTO_CONDENSED_BOLD_ITALIC = 15;
-    /**
-     * List of created typefaces for later reused.
-     */
+
     private final static SparseArray<Typeface> mTypefaces = new SparseArray<Typeface>(16);
 
-    /**
-     * Simple constructor to use when creating a view from code.
-     *
-     * @param context The Context the view is running in, through which it can
-     *                access the current theme, resources, etc.
-     */
     public RobotoTextView(Context context) {
         super(context);
     }
 
-    /**
-     * Constructor that is called when inflating a view from XML. This is called
-     * when a view is being constructed from an XML file, supplying attributes
-     * that were specified in the XML file. This version uses a default style of
-     * 0, so the only attribute values applied are those in the Context's Theme
-     * and the given AttributeSet.
-     * <p/>
-     * <p/>
-     * The method onFinishInflate() will be called after all children have been
-     * added.
-     *
-     * @param context The Context the view is running in, through which it can
-     *                access the current theme, resources, etc.
-     * @param attrs   The attributes of the XML tag that is inflating the view.
-     * @see #RobotoTextView(android.content.Context, android.util.AttributeSet, int)
-     */
     public RobotoTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
         parseAttributes(context, attrs);
     }
 
-    /**
-     * Perform inflation from XML and apply a class-specific base style. This
-     * constructor of View allows subclasses to use their own base style when
-     * they are inflating.
-     *
-     * @param context  The Context the view is running in, through which it can
-     *                 access the current theme, resources, etc.
-     * @param attrs    The attributes of the XML tag that is inflating the view.
-     * @param defStyle The default style to apply to this view. If 0, no style
-     *                 will be applied (beyond what is included in the theme). This may
-     *                 either be an attribute resource, whose value will be retrieved
-     *                 from the current theme, or an explicit style resource.
-     * @see #RobotoTextView(android.content.Context, android.util.AttributeSet)
-     */
     public RobotoTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         parseAttributes(context, attrs);
     }
 
-    /**
-     * Parse the attributes.
-     *
-     * @param context The Context the view is running in, through which it can access the current theme, resources, etc.
-     * @param attrs   The attributes of the XML tag that is inflating the view.
-     */
     private void parseAttributes(Context context, AttributeSet attrs) {
         TypedArray values = context.obtainStyledAttributes(attrs, R.styleable.RobotoTextView);
 
@@ -100,15 +52,6 @@ public class RobotoTextView extends TextView {
         setTypeface(obtaintTypeface(context, typefaceValue));
     }
 
-    /**
-     * Obtain typeface.
-     *
-     * @param context       The Context the view is running in, through which it can
-     *                      access the current theme, resources, etc.
-     * @param typefaceValue values ​​for the "typeface" attribute
-     * @return Roboto {@link android.graphics.Typeface}
-     * @throws IllegalArgumentException if unknown `typeface` attribute value.
-     */
     private Typeface obtaintTypeface(Context context, int typefaceValue) throws IllegalArgumentException {
         Typeface typeface = mTypefaces.get(typefaceValue);
         if (typeface == null) {
@@ -118,15 +61,6 @@ public class RobotoTextView extends TextView {
         return typeface;
     }
 
-    /**
-     * Create typeface from assets.
-     *
-     * @param context       The Context the view is running in, through which it can
-     *                      access the current theme, resources, etc.
-     * @param typefaceValue values ​​for the "typeface" attribute
-     * @return Roboto {@link android.graphics.Typeface}
-     * @throws IllegalArgumentException if unknown `typeface` attribute value.
-     */
     private Typeface createTypeface(Context context, int typefaceValue) throws IllegalArgumentException {
         Typeface typeface;
         switch (typefaceValue) {
@@ -183,5 +117,4 @@ public class RobotoTextView extends TextView {
         }
         return typeface;
     }
-
 }
