f9e5db6873829fc75cbdcbf3a2c29f902a760ebe
Florian <florian.schwanz@interoberlin.de>
2015-05-28 00:16:19 +0200

    Created super class for auto-hide

--- app/src/main/java/de/interoberlin/lymbo/view/activities/LymbosActivity.java
+++ app/src/main/java/de/interoberlin/lymbo/view/activities/LymbosActivity.java
@@ -9,16 +9,10 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.animation.DecelerateInterpolator;
-import android.widget.AbsListView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 
 import com.fortysevendeg.swipelistview.SwipeListView;
 import com.github.mrengineer13.snackbar.SnackBar;
-
-import java.util.ArrayList;
 
 import de.interoberlin.lymbo.R;
 import de.interoberlin.lymbo.controller.CardsController;
@@ -30,7 +24,7 @@ import de.interoberlin.lymbo.view.dialogfragments.DisplayDialogFragment;
 import de.interoberlin.mate.lib.view.AboutActivity;
 import de.interoberlin.mate.lib.view.LogActivity;
 
-public class LymbosActivity extends BaseActivity implements SwipeRefreshLayout.OnRefreshListener, SnackBar.OnMessageClickListener, DisplayDialogFragment.OnCompleteListener {
+public class LymbosActivity extends SwipeRefreshBaseActivity implements SwipeRefreshLayout.OnRefreshListener, SnackBar.OnMessageClickListener, DisplayDialogFragment.OnCompleteListener {
     // Controllers
     private LymbosController lymbosController = LymbosController.getInstance();
     private CardsController cardsController = CardsController.getInstance();
@@ -42,18 +36,6 @@ public class LymbosActivity extends BaseActivity implements SwipeRefreshLayout.O
 
     // Model
     private LymbosListAdapter lymbosAdapter;
-
-    // Toolbar
-    private ArrayList<View> hideableHeaderViews = new ArrayList<>();
-    private ArrayList<View> hideableFooterViews = new ArrayList<>();
-
-    private boolean actionBarAutoHideEnabled = false;
-    private int actionBarAutoHideSensivity = 0;
-    private int actionBarAutoHideMinY = 0;
-    private int actionBarAutoHideSignal = 0;
-    private boolean actionBarShown = true;
-
-    private static final int HEADER_HIDE_ANIM_DURATION = 300;
 
     private static int REFRESH_DELAY;
 
@@ -69,6 +51,7 @@ public class LymbosActivity extends BaseActivity implements SwipeRefreshLayout.O
             lymbosController.load();
         }
 
+        setSwipeRefreshLayout(srl);
         setActionBarIcon(R.drawable.ic_ab_drawer);
         setDisplayHomeAsUpEnabled(false);
 
@@ -223,137 +206,6 @@ public class LymbosActivity extends BaseActivity implements SwipeRefreshLayout.O
     @Override
     public void onDiscardCardDialogComplete() {
 
-    }
-
-    // --------------------
-    // Methods -  Toolbar
-    // --------------------
-
-    /**
-     * Initializes the Action Bar auto-hide (aka Quick Recall) effect.
-     */
-    private void initActionBarAutoHide() {
-        actionBarAutoHideEnabled = true;
-        actionBarAutoHideMinY = getResources().getDimensionPixelSize(
-                R.dimen.toolbar_auto_hide_min_y);
-        actionBarAutoHideSensivity = getResources().getDimensionPixelSize(
-                R.dimen.toolbar_auto_hide_sensivity);
-    }
-
-    protected void enableActionBarAutoHide(final ListView listView) {
-        initActionBarAutoHide();
-        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
-            final static int ITEMS_THRESHOLD = 1;
-            int lastFvi = 0;
-
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-            }
-
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                onMainContentScrolled(firstVisibleItem <= ITEMS_THRESHOLD ? 0 : Integer.MAX_VALUE,
-                        lastFvi - firstVisibleItem > 0 ? Integer.MIN_VALUE :
-                                lastFvi == firstVisibleItem ? 0 : Integer.MAX_VALUE
-                );
-                lastFvi = firstVisibleItem;
-            }
-        });
-    }
-
-    /**
-     * Indicates that the main content has scrolled (for the purposes of showing/hiding
-     * the action bar for the "action bar auto hide" effect). currentY and deltaY may be exact
-     * (if the underlying view supports it) or may be approximate indications:
-     * deltaY may be INT_MAX to mean "scrolled forward indeterminately" and INT_MIN to mean
-     * "scrolled backward indeterminately".  currentY may be 0 to mean "somewhere close to the
-     * start of the list" and INT_MAX to mean "we don't know, but not at the start of the list"
-     */
-    private void onMainContentScrolled(int currentY, int deltaY) {
-        if (deltaY > actionBarAutoHideSensivity) {
-            deltaY = actionBarAutoHideSensivity;
-        } else if (deltaY < -actionBarAutoHideSensivity) {
-            deltaY = -actionBarAutoHideSensivity;
-        }
-
-        if (Math.signum(deltaY) * Math.signum(actionBarAutoHideSignal) < 0) {
-            // deltaY is a motion opposite to the accumulated signal, so reset signal
-            actionBarAutoHideSignal = deltaY;
-        } else {
-            // add to accumulated signal
-            actionBarAutoHideSignal += deltaY;
-        }
-
-        boolean shouldShow = currentY < actionBarAutoHideMinY ||
-                (actionBarAutoHideSignal <= -actionBarAutoHideSensivity);
-        autoShowOrHideActionBar(shouldShow);
-    }
-
-    protected void autoShowOrHideActionBar(boolean show) {
-        if (show == actionBarShown) {
-            return;
-        }
-
-        actionBarShown = show;
-        onActionBarAutoShowOrHide(show);
-    }
-
-    protected void onActionBarAutoShowOrHide(boolean shown) {
-        updateSwipeRefreshProgressBarTop();
-
-        for (View view : hideableHeaderViews) {
-            if (shown) {
-                view.animate()
-                        .translationY(0)
-                        .alpha(1)
-                        .setDuration(HEADER_HIDE_ANIM_DURATION)
-                        .setInterpolator(new DecelerateInterpolator());
-            } else {
-                view.animate()
-                        .translationY(-view.getBottom())
-                        .alpha(0)
-                        .setDuration(HEADER_HIDE_ANIM_DURATION)
-                        .setInterpolator(new DecelerateInterpolator());
-            }
-        }
-    }
-
-    private void updateSwipeRefreshProgressBarTop() {
-        if (srl == null) {
-            return;
-        }
-
-        int progressBarStartMargin = getResources().getDimensionPixelSize(
-                R.dimen.swipe_refresh_progress_bar_start_margin);
-        int progressBarEndMargin = getResources().getDimensionPixelSize(
-                R.dimen.swipe_refresh_progress_bar_end_margin);
-        int progressBarTopWhenActionBarShown = getResources().getDimensionPixelSize(R.dimen.toolbar_height);
-        int top = actionBarShown ? progressBarTopWhenActionBarShown : 0;
-        srl.setProgressViewOffset(false, top + progressBarStartMargin, top + progressBarEndMargin);
-    }
-
-    protected void registerHideableHeaderView(View hideableHeaderView) {
-        if (!hideableHeaderViews.contains(hideableHeaderView)) {
-            hideableHeaderViews.add(hideableHeaderView);
-        }
-    }
-
-    protected void deregisterHideableHeaderView(View hideableHeaderView) {
-        if (hideableHeaderViews.contains(hideableHeaderView)) {
-            hideableHeaderViews.remove(hideableHeaderView);
-        }
-    }
-
-    protected void registerHideableFooterView(View hideableFooterView) {
-        if (!hideableFooterViews.contains(hideableFooterView)) {
-            hideableFooterViews.add(hideableFooterView);
-        }
-    }
-
-    protected void deregisterHideableFooterView(View hideableFooterView) {
-        if (hideableFooterViews.contains(hideableFooterView)) {
-            hideableFooterViews.remove(hideableFooterView);
-        }
     }
 
     // --------------------
