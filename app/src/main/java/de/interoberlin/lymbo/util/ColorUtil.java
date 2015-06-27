59df5a258c7afb2a21087ef4ec7a5129dc8264a2
Florian <florian.schwanz@interoberlin.de>
2015-06-27 16:44:55 +0200

    Added string to color mapping

---
+++ app/src/main/java/de/interoberlin/lymbo/util/ColorUtil.java
@@ -0,0 +1,14 @@
+package de.interoberlin.lymbo.util;
+
+import android.content.Context;
+
+import de.interoberlin.lymbo.R;
+
+public class ColorUtil {
+
+    public static int getColorByString(Context c, String value) {
+        int[] colors = c.getResources().getIntArray(R.array.tag_color);
+
+        return colors[value.hashCode() % colors.length];
+    }
+}
